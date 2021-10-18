d457cf63d9f7067e1899bf638d72855623b59614
Tat Vi Quyen <tvq9612@gmail.com>
2021-10-18 16:08:40 +0700

    external ship

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/shopeefood',
+        destination:
+          'https://shopeefood.vn/ho-chi-minh/diem-tam-su-ky-ha-cao-xiu-mai-au-co.gvrkd5',
+        permanent: true,
+      },
+      {
+        source: '/shopeefoodtp',
+        destination:
+          'https://shopeefood.vn/ho-chi-minh/diem-tam-su-ky-thuc-pham-dong-lanh-au-co',
+        permanent: true,
+      },
+      {
+        source: '/loship',
+        destination: 'https://loship.vn/diemtamsuky',
+        permanent: true,
+      },
+    ]
+  },
 }
